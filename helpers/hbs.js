aeb713ab746dd56aafa5fcab0ef75ce6bfb924da
Ved Prakash <55890729+vedarya1@users.noreply.github.com>
2021-01-05 14:54:25 +0530

    Update hbs.js

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -1,10 +1,10 @@
 const moment = require('moment')
 
 module.exports = {
-	formatDate: function (date, format) {
-		return moment(date).utc().format(format)
-	},
-   	 truncate: function (str, len) {
+  formatDate: function (date, format) {
+    return moment(date).utc().format(format)
+  },
+  truncate: function (str, len) {
     if (str.length > len && str.length > 0) {
       let new_str = str + ' '
       new_str = str.substr(0, len)
@@ -17,7 +17,7 @@ module.exports = {
   stripTags: function (input) {
     return input.replace(/<(?:.|\n)*?>/gm, '')
   },
-   editIcon: function (storyUser, loggedUser, storyId, floating = true) {
+  editIcon: function (storyUser, loggedUser, storyId, floating = true) {
     if (storyUser._id.toString() == loggedUser._id.toString()) {
       if (floating) {
         return `<a href="/stories/edit/${storyId}" class="btn-floating halfway-fab blue"><i class="fas fa-edit fa-small"></i></a>`
@@ -28,7 +28,7 @@ module.exports = {
       return ''
     }
   },
-   select: function (selected, options) {
+  select: function (selected, options) {
     return options
       .fn(this)
       .replace(
